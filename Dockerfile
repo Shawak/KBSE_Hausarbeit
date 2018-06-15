558c8e844cecff05f5e155a856500ea846b0d958
Shawak <maxi.nussbaum@googlemail.com>
2018-06-15 23:03:50 +0200

    docker container is now useable

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,9 @@
 ﻿FROM oracle/glassfish
 
+COPY docker-entrypoint.sh /_entrypoint.sh
+RUN sed -i 's/\r//' _entrypoint.sh
+ENTRYPOINT ["/_entrypoint.sh"]
+
 EXPOSE 4848 8080
+
+CMD ["asadmin", "start-domain", "-v"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+# 
+# Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved.
+if [[ -z $ADMIN_PASSWORD ]]; then
+  ADMIN_PASSWORD=$(date| md5sum | fold -w 8 | head -n 1)
+  echo "##########GENERATED ADMIN PASSWORD: $ADMIN_PASSWORD  ##########"
+fi
+echo "AS_ADMIN_PASSWORD=" > /tmp/glassfishpwd
+echo "AS_ADMIN_NEWPASSWORD=${ADMIN_PASSWORD}" >> /tmp/glassfishpwd
+asadmin --user=admin --passwordfile=/tmp/glassfishpwd change-admin-password --domain_name domain1
+asadmin start-domain
+echo "AS_ADMIN_PASSWORD=${ADMIN_PASSWORD}" > /tmp/glassfishpwd
+asadmin --user=admin --passwordfile=/tmp/glassfishpwd enable-secure-admin
+
+# create the connection pool by cli
+asadmin start-database
+asadmin --user=admin --passwordfile=/tmp/glassfishpwd create-jdbc-connection-pool --datasourceclassname org.apache.derby.jdbc.ClientDataSource --restype javax.sql.DataSource --property portNumber=1527:password=Administrator:user=Administrator:serverName=localhost:databaseName=SuperCar:connectionAttributes=\;create\\=true SuperCarPool
+
+# ping it once to trigger the create=true
+asadmin --user=admin --passwordfile=/tmp/glassfishpwd ping-connection-pool SuperCarPool
+
+# deploy
+cp /app/Hausarbeit-1.0.war /glassfish5/glassfish/domains/domain1/autodeploy/Hausarbeit.war
+
+asadmin --user=admin stop-domain
+rm /tmp/glassfishpwd
+exec "$@"
