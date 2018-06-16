61c25f02a7e763995255210aad026258dad428fd
Lukas <Lukas@Prophet>
2018-06-16 16:04:27 +0200

    Repositorys angelegt

---
+++ supercar/src/main/java/supercar/repositories/GarageRepository.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package supercar.repositories;
+
+import javax.ejb.Stateless;
+import supercar.entities.Garage;
+import supercar.interfaces.IRepository;
+
+/**
+ *
+ * @author Lukas
+ */
+@Stateless
+public class GarageRepository extends IRepository<Garage> {
+
+    public GarageRepository() {
+    }
+    
+}
