d7a2400c4ad4f0be8ca5131c21000060bfdca04c
Shawak <maxi.nussbaum@googlemail.com>
2018-06-23 18:24:42 +0200

    add PlzApi

---
+++ supercar/src/main/java/supercar/core/PlzApi.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package supercar.core;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonArray;
+
+/**
+ *
+ * @author Maxi
+ * http://blog.wilderland.de/2015/05/webservice-fur-die-abfrage-von-postleitzahlen/
+ */
+public class PlzApi {
+    
+    protected Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    
+    public String getName(int plz) {
+        String json = WebHelper.getHTML("http://api.zippopotam.us/de/" + plz);
+        JsonObject obj = gson.fromJson(json, JsonObject.class);
+        if (obj == null) {
+            return null;
+        }
+        
+        JsonArray arr = obj.getAsJsonArray("places");
+        return arr.size() > 0 ? arr.get(0).getAsJsonObject().get("place name").toString() : null;
+    }
+}
---
+++ supercar/src/main/java/supercar/core/WebHelper.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package supercar.core;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ *
+ * @author Maxi
+ */
+public class WebHelper {
+    
+     public static String getHTML(String urlToRead) {
+        try {
+            StringBuilder result = new StringBuilder();
+            URL url = new URL(urlToRead);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+            while ((line = rd.readLine()) != null) {
+               result.append(line);
+            }
+            rd.close();
+            return result.toString();
+        } catch (FileNotFoundException ex) {
+        } catch (Exception ex) {
+           System.out.println(ex);
+        }
+        return null;
+   }
+     
+}
