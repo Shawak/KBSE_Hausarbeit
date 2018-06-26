6f554024bde02b84995a740eccb98fd08619a15a
Patrick <Patrick@DESKTOP-JATVFE7>
2018-06-26 20:28:32 +0200

    Bilder für Autos hinzugefügt

--- supercar/src/main/java/supercar/core/StartupBean.java
+++ supercar/src/main/java/supercar/core/StartupBean.java
@@ -5,12 +5,26 @@
  */
 package supercar.core;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.ejb.Startup;
 import javax.ejb.Singleton;
 import supercar.entities.*;
 import supercar.enums.AccountType;
 import supercar.interfaces.IRepositoryAccessor;
+
+import org.apache.commons.io.IOUtils;
 
 /**
  *
@@ -46,8 +60,8 @@ public class StartupBean extends IRepositoryAccessor {
             Model v70 = Models.add(new Model("V70", volvo, 1, 1));
             Model granTurismo = Models.add(new Model("GranTurismo", maserati, 1, 1));
             
-            Cars.add(new Car("OS HS 0001", 40.0f, astra, "", ""));
-            Cars.add(new Car("OS HS 0002", 50.0f, sprinter, "", ""));
+            Cars.add(new Car("OS HS 0001", 40.0f, astra, "Opel_Astra.jpg", "gray"));
+            Cars.add(new Car("OS HS 0002", 50.0f, sprinter, "Merceds-Benz_Sprinter.jpg", "white"));
             Cars.add(new Car("OS HS 0003", 120.0f, aventador, "", ""));
             Cars.add(new Car("OS HS 0004", 31.0f, gtr, "", ""));
             Cars.add(new Car("OS HS 0005", 60.5f, kangoo, "", ""));
@@ -55,6 +69,42 @@ public class StartupBean extends IRepositoryAccessor {
             Cars.add(new Car("OS HS 0007", 75.31f, aygo, "", ""));
             Cars.add(new Car("OS HS 0008", 65.7f, v70, "", ""));
             Cars.add(new Car("OS HS 0009", 21.5f, granTurismo, "", ""));
+            
+            //Bilder speichern
+            List<String> bilder = new ArrayList<>();
+            
+            bilder.add("Opel_Astra.jpg");
+            bilder.add("Mercedes-Benz_Sprinter.jpg");
+            
+            URL location;
+            location = StartupBean.class.getProtectionDomain().getCodeSource().getLocation();
+            String path = location.getPath();
+            path = path.substring(1, path.length()-17);
+            for(String e: bilder){
+                try {
+                    InputStream inputFile;
+                    inputFile = new FileInputStream(path+"../../../../../classes/"+e);
+                    byte[] bytes;
+                    bytes = IOUtils.toByteArray(inputFile);
+                    
+                    File file = new File(path+"../../../../images/"+e);
+                    if(!file.exists()){
+                        file.createNewFile();
+                    }
+                    
+                    OutputStream outputFile;
+                    outputFile = new FileOutputStream(file);
+                    outputFile.write(bytes);
+                    outputFile.flush();
+                    outputFile.close();
+                } catch (FileNotFoundException ex) {
+                    Logger.getLogger(StartupBean.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IOException ex) {
+                    Logger.getLogger(StartupBean.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            
+            
         }
     }
     
