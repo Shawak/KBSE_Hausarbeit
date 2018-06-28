bf4e26250f3437aded0902eefd51738095821a17
Shawak <maxi.nussbaum@googlemail.com>
2018-06-28 19:30:58 +0200

    add filters

---
+++ supercar/src/main/java/supercar/filters/EmployeeFilter.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package supercar.filters;
+
+import supercar.enums.AccountType;
+import supercar.interfaces.IFilter;
+
+/**
+ *
+ * @author Maxi
+ */
+public class EmployeeFilter extends IFilter {
+    
+    public EmployeeFilter() {
+        this.filter = () -> LoginHandler.hasAccess(AccountType.Employee);
+    }
+    
+}
---
+++ supercar/src/main/java/supercar/interfaces/IFilter.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package supercar.interfaces;
+
+import java.io.IOException;
+import java.util.function.Supplier;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Maxi
+ */
+public abstract class IFilter extends IRestrictableRepositoryAccessor implements Filter {
+    
+    protected Supplier<Boolean> filter;
+    
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest)servletRequest;
+        HttpServletResponse response = (HttpServletResponse)servletResponse;
+        
+        if (this.filter.get()) {
+            chain.doFilter(request, response);
+        } else {
+            String needLoginUrl = request.getContextPath() + "/faces/need_login.xhtml";
+            response.sendRedirect(needLoginUrl);
+        }
+    }
+    
+}
