74a09539df62c0cff69601f2d35c12e5506c4d46
Shawak <maxi.nussbaum@googlemail.com>
2018-06-28 19:33:04 +0200

    add url

--- supercar/src/main/java/supercar/interfaces/IFilter.java
+++ supercar/src/main/java/supercar/interfaces/IFilter.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  *
  * @author Maxi
+ * http://www.catgovind.com/jsf/jsf-login-logout-form-authentication-example/
  */
 public abstract class IFilter extends IRestrictableRepositoryAccessor implements Filter {
     
