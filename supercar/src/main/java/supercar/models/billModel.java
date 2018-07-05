a2a88d59390251c7f4a847f0c85cee1ca6447522
Lukas <Lukas@Prophet>
2018-07-05 17:15:03 +0200

    PDF Rechnung ohne Ausleihe mit Testbutton auf about Seite

---
+++ supercar/src/main/java/supercar/models/billModel.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package supercar.models;
+
+import com.itextpdf.text.DocumentException;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import supercar.core.billPDF;
+import supercar.entities.Account;
+import supercar.interfaces.IModel;
+
+/**
+ *
+ * @author Lukas
+ */
+@Named("bill")
+@SessionScoped
+public class billModel extends IModel {
+    @Inject
+    billPDF bill;
+    
+    public void test() throws FileNotFoundException, DocumentException
+    {
+        URL location;
+        location = billModel.class.getProtectionDomain().getCodeSource().getLocation();
+        String path = location.getPath();
+        Account acc = LoginHandler.getAccount();
+        bill.createPDF(path+ "../../../../../../images/test.pdf", acc);
+    }
+}
